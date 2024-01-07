8d52f74231d46289b66e306e88386caae1382293
ahsanalsifat <ahsanalsifat@gmail.com>
2024-01-07 21:08:51 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,19 @@
+let okay = prompt("User 1");
+
+let yes = prompt("User 2");
+okay = parseInt(okay);
+yes = parseFloat(yes);
+
+const result1 = okay + yes;
+const result2 = okay - yes;
+const result3 = okay / yes;
+const result4 = okay * yes;
+const result5 = okay % yes;
+
+
+document.write("output1:"+ result1 + "<br>");
+document.write("output2:"+ result2 + "<br>");
+document.write("output3:"+ result3 + "<br>");
+document.write("output4:"+ result4 + "<br>");
+document.write("output5:"+ result5 + "<br>");
+
